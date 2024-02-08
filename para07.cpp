0d4694ecfbc449dc50a71cf9d566ebab6f5aedf8
ethnimf <144279943+ethnimf@users.noreply.github.com>
2024-02-08 08:14:54 +0300

    Add files via upload

---
+++ para07.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+using namespace std;
+
+double Circle(double radius) {
+    return 3.14 * radius * radius;
+}
+
+double Ring(double innerRadius, double outerRadius) 
+{
+    
+    if (innerRadius >= outerRadius) {
+        cout << "Error: The inner radius must be less than the outer radius." << endl;
+        return 0.0;
+    }
+
+    double ringArea = Circle(outerRadius) - Circle(innerRadius);
+
+    return ringArea;
+}
+
+int main() {
+    double innerRadius, outerRadius;
+
+    cout << "Enter the inner radius of the ring: ";
+    cin >> innerRadius;
+    cout << "Enter the outer radius of the ring: ";
+    cin >> outerRadius;
+
+    double ringArea = Ring(innerRadius, outerRadius);
+
+    cout << "Area of the ring: " << ringArea << endl;
+
+    return 0;
+}
